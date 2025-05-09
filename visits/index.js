6f31d55dc895ead970b61705f4918de2498ec4a5
gagan <gasingla07@gmail.com>
2025-05-09 13:50:33 -0500

    update redis code to version 4.7.0

--- visits/index.js
+++ visits/index.js
@@ -1,24 +1,22 @@
 const express = require("express");
 const redis = require("redis");
-const process = require("process");
 
 const app = express();
-
 const client = redis.createClient({
-  host: "redis-server",
-  port: 6379,
+  url: "redis://redis-server:6379",
 });
 
-client.set("visits", 0);
+(async () => {
+  await client.connect();
+  await client.set("visits", 0);
+})();
 
-app.get("/", (req, res) => {
-  process.exit(0);
-  client.incr("visits", (err, visits) => {
-    res.send("Number of visits is " + visits);
-    //    client.set("visits", parseInt(visits) + 1);
-  });
+app.get("/", async (req, res) => {
+  const visits = await client.get("visits");
+  res.send("Number of visits " + visits);
+  await client.set("visits", parseInt(visits) + 1);
 });
 
 app.listen(8081, () => {
-  console.log("Listening on Port 8081");
+  console.log("listening on port 8081");
 });
